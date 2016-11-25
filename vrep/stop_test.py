c34f59832969a1edfac3374930c730aae6f492ab
creator <creator@mr16.local>
2016-11-25 15:09:26 +0900

    param

--- vrep/stop_test.py
+++ vrep/stop_test.py
@@ -23,7 +23,7 @@ while True:
             x, y, w, h, im = rmb.detect_col("red")
             print x, w
             time.sleep(0.5)
-            rmb.turn_angle(rmb.right,90)
+            rmb.turn_angle(rmb.right,360)
             count+=1
 
     time.sleep(0.1)
